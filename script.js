7411a6419e2175582e955f64952e97f9c7ad8a2a
Anas Bin Sohail <59366780+thebinsohail@users.noreply.github.com>
2020-04-15 22:36:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function find() {
+  user=document.getElementById('username').value;
+  repository="Total Public Repositories : ";
+  followers="Total Followers : ";
+  about="About\n"
+    fetch("https://api.github.com/users/"+user)
+    .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        //console.log(data);
+        document.getElementById('user-image').src = data.avatar_url;
+        document.getElementById('name').innerHTML=data.name;
+        document.getElementById('bio').innerHTML=data.bio;
+        document.getElementById('repos').innerHTML=repository+data.public_repos;
+        document.getElementById('followers').innerHTML=followers+data.followers;
+        document.getElementById('link').innerHTML=data.url;
+      });
+
+    
+   
+}
+
